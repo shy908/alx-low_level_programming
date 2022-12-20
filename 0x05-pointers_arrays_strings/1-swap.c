d9385a6e5289206bc16ac268a34ea7651d9a2dae
shy908 <manuelshylet@gmail.com>
2022-12-20 13:33:38 -0800

    swap

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * swap_int - this function change the
+ * initial.
+ * @a: this is a parameter to change.
+ * @b: this is the another parameter
+ * to change.
+ */
+
+void swap_int(int *a, int *b)
+{
+	int t = *a;
+	*a = *b;
+	*b = t;
+}
