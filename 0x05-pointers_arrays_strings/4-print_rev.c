a2b5e0567f36907c80783b0a043e5f651fcd5b62
shy908 <manuelshylet@gmail.com>
2022-12-20 17:39:33 -0800

    print rev

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_rev - the characters print in
+ * reverse.
+ * @s: this is a parameter to change
+ */
+
+void print_rev(char *s)
+{
+	int i = 0;
+
+	while (s[i] != 0)
+		i++;
+	i--;
+	while (i >= 0)
+	{
+		_putchar(s[i]);
+		i--;
+	}
+	_putchar('\n');
+}
