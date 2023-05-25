d88fb23b74e7a2815f34d839ac91574d8173bd1e
shy908 <manuelshylet@gmail.com>
2023-05-24 17:39:01 -0700

    update

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,19 +1,37 @@
 #!/usr/bin/python3
+"""
+Task 5 Return the perimeter of the island described in grid.
+"""
 
 
 def island_perimeter(grid):
-    """Python function to return the perimeter of an island"""
-
+    """Method tha returns the perimeter of the island described in grid."""
+    # p = perimeter, i = iteration 1, j = iteration2
     p = 0
-    for x in range(len(grid)):
-        for y in range(len(grid[x])):
-            if grid[x][y] == 1:
-                if x + 1 == len(grid) or grid[x + 1][y] == 0:
+    for i in range(len(grid)):
+        for j in range(len(grid[0])):
+            if grid[i][j] == 1:
+                try:
+                    if grid[i - 1][j] == 0 or i == 0:
+                        p += 1
+                except IndexError:
                     p += 1
-                if y + 1 == len(grid[0]) or grid[x][y + 1] == 0:
+
+                try:
+                    if grid[i + 1][j] == 0:
+                        p += 1
+                except IndexError:
                     p += 1
-                if x - 1 == -1 or grid[x - 1][y] == 0:
+
+                try:
+                    if grid[i][j - 1] == 0 or j == 0:
+                        p += 1
+                except IndexError:
                     p += 1
-                if y - 1 == -1 or grid[x][y - 1] == 0:
+
+                try:
+                    if grid[i][j + 1] == 0:
+                        p += 1
+                except IndexError:
                     p += 1
-    return(p)
+    return p
